5c5324f4f8491070be3ad3029e7f250e477b8ee5
Samuel Mallia <mallia.3@wright.edu>
2026-04-06 17:12:40 -0400

    CF Template and DockerSwap script

---
+++ deployment/DockerSwap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Pull a docker image and replace the currently running container with the new image
+# intended to be placed in /usr/local/bin called DockerSwap with execute permissions for the ec2-user
+
+# Usage DockerSwap -i <dockerRepo/image:tag> -c <containerName>
+
+# Docker Path to pull from
+image=""
+
+# Name of local container to replace
+containerName=""
+
+getops ":i:c:" opt
+while getopts ":i:c:" opt; do
+    case $opt in
+        i) image="$OPTARG"
+        ;;
+        c) containerName="$OPTARG"
+        ;;
+        \?) echo "Invalid option -$OPTARG" >&2
+        ;;
+    esac
+done
+
+# Pull the latest image from the repository
+docker pull ${image}
+
+# Stop the currently running container
+docker stop ${containerName}
+
+# Remove the stopped container
+docker rm ${containerName}
+
+# Run the new container
+docker run -d --name ${containerName} -p 80:80 ${image}
+
+exit 0
