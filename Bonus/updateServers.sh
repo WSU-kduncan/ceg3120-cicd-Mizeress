86845ba972b2c76d0cf10379c561b319ac3af321
Samuel Mallia <mallia.3@wright.edu>
2026-04-13 14:54:44 -0400

    Update Servers and SSH Config

--- Bonus/updateServers.sh
+++ Bonus/updateServers.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 # Use SSH to call the update script on the servers
+# Remember to manually add your PK to the proxy server
+servers=("webserv1" "webserv2" "webserv3") # Replace with your SSH config names
+
+for server in "${servers[@]}"; do
+    echo "Updating $server..."
+    ssh "$server" 'bash -s' < DockerSwap -i malliasm/portfolio -c portfolio
+done
